849025dcbe0aecf6ae1d10843f73568d182815dc
Fei Xia (from Dev Box) <feixia@microsoft.com>
2024-02-06 12:55:50 +0800

    456

--- server.mjs
+++ server.mjs
@@ -43,28 +43,28 @@ app.get('/send-notificationc', async (req, res) => {
     
     data.forEach(element => {
 
-      try {
-        
-        let pushSubscription = {
-          endpoint: undefined,
-          keys: {
-            auth: undefined,
-            p256dh: undefined,
-          },
-        };
-        pushSubscription = element.subscription;
-         webpush.sendNotification(pushSubscription, JSON.stringify({
-          title: "Hello World",
-          body: "Notification sent from backend"
-        }));
-        res.sendStatus(200);
 
-      } catch(err) {
-        console.error(err);
-        res.sendStatus(500);
-      }
     });
-  
+    try {
+        
+      let pushSubscription = {
+        endpoint: "https://wns2-by3p.notify.windows.com/w/?token=BQYAAABfeaWNsd0vT9hGGVmGeRCTrFN%2ftHMG%2fIPgXz4pt364OtQmM42HUN7%2b0AWaivB23DXwLLuAq6IHespNQb8BCM7pTqridw99K8c0dQ2j1bT0IbKhuHThzAhdOAhHSSzeudD0U3TDtMrdUjyHm901j65sLuPSkivea3yK1K0pS1mjilAr%2bW6MuhZpBrFP1D9T38uet0FJawwwgHUmlMeShLng6RAzHQInRsAmcN7v2DkgqgB3JpLbEaFZHu2qLNVP%2bOUsOcGNnSewo4ZpLyhoY97Ot3vHZv8Ndwnz%2batM7TxYWSUo%2b5QINnlobIOfim4vVSY%3d",
+        keys: {
+          auth: "OCvUtWVfG9xHjqsuvEiuxw",
+          p256dh: "BKIozGTIkh7C-MCsENPw9S35nTY0_nPC8_dwMkA3bCKlHjb1kxzQu3bPIDl4bD3toKaYr26tEYmzOh5cd-06KMc",
+        },
+      };
+      //pushSubscription = element.subscription;
+       webpush.sendNotification(pushSubscription, JSON.stringify({
+        title: "Hello World",
+        body: "Notification sent from backend"
+      }));
+      res.sendStatus(200);
+
+    } catch(err) {
+      console.error(err);
+      res.sendStatus(500);
+    }
   
   }).catch(error => console.error(error));
 
