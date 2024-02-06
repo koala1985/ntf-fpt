e87d3f67d5d43179924a927e10cc0e5e5f17bba4
Fei Xia (from Dev Box) <feixia@microsoft.com>
2024-02-06 15:33:14 +0800

    testa

--- server.mjs
+++ server.mjs
@@ -9,7 +9,7 @@ const app = express();
 app.use(express.json());
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://feilabfpt.azurewebsites.net');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
 });
